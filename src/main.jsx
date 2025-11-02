1fb8a3bb5bfb4d9885b752fc5f8247ff3c11f670
Praveen Kumar <praveensofficial1@gmail.com>
2025-11-02 17:35:17 +0530

    Updated

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes/route";
+import router from "./routes/Route";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "react-hot-toast";
 import { AuthProvider } from "./contexts/AuthContext";
